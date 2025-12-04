ce8e67445c2fef3d6b5d93d95cfaf340b5a04356
Mykyta <nikita.borodin1212@gmail.com>
2025-12-04 11:18:05 +0200

    commit

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -4,7 +4,6 @@ import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import { fetchNoteById } from "@/lib/api";
 import Modal from "@/app/@modal/Modal";
-import type { Note } from "@/types/note";
 
 interface NotePreviewProps {
   noteId: string;
@@ -13,10 +12,10 @@ interface NotePreviewProps {
 export default function NotePreviewClient({ noteId }: NotePreviewProps) {
   const router = useRouter();
 
-  const { data: note, isError, isLoading } = useQuery<Note>({
+  const { data: note, isError, isLoading } = useQuery({
     queryKey: ["note", noteId],
     queryFn: () => fetchNoteById(noteId),
-    refetchOnMount: true,
+    refetchOnMount: true
   });
 
   if (isLoading || isError || !note) return null;
@@ -25,7 +24,6 @@ export default function NotePreviewClient({ noteId }: NotePreviewProps) {
     <Modal onClose={() => router.back()}>
       <h2>{note.title}</h2>
       <p>{note.content}</p>
-
       <button onClick={() => router.back()}>Close</button>
     </Modal>
   );
